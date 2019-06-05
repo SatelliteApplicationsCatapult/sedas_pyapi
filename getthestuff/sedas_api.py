d30effaa7388cadafebe6d077436d0f702c3259d
Wil Selwood <wil.selwood@sa.catapult.org.uk>
2019-06-05 11:21:29 +0100

    Better error handling, relogin, and docs.
    Add: Automatic relogging in if the token is likely to have expired.
    Mod: Updated doc strings for builk downloader.
    Add: Type hints to most functions.
    Mov: Example to the bulk_download.py from sedas_api.py to avoid a import loop.
    Add: Basic retry logic on authorisation errors.

--- getthestuff/sedas_api.py
+++ getthestuff/sedas_api.py
@@ -1,11 +1,12 @@
 import json
+import logging
 import shutil
 import time
-from getpass import getpass
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 
-from getthestuff.bulk_download import SeDASBulkDownload
+_logger = logging.getLogger("sedas_api")
+
 
 class SeDASAPI:
     """
@@ -13,27 +14,33 @@ class SeDASAPI:
 
     Create an instance of this object providing your username and password.
     Then use this class to search for data or download data.
-
-    See the main at the end of this file for examples of how to use this.
     """
     base_url = "https://geobrowser.satapps.org/api/"
     authentication_url = f"{base_url}authentication"
     search_url = f"{base_url}search"
     headers = {"Content-Type": "application/json", "Authorization": None}
 
-    _token = None
+    token_time_out = 60 * 20  # 20 minutes
 
-    def __init__(self, _username, __password):
+    _token = None
+    _token_time = None
+
+    def __init__(self, _username: str, __password: str) -> None:
         self._username = _username
         self.__password = __password
         self.login()
 
-    def login(self):
+    def login(self) -> None:
         """
         Log into the sedas platform.
 
         :return: access token if login was successful.
         """
+        # if we already have a token and it is not likely to have expired yet we can skip the rest
+        # of the login process
+        if self._token and (self._token_time and time.time() - self._token_time < self.token_time_out):
+            return
+
         data = {'username': self._username, 'password': self.__password}
 
         req = Request(
@@ -44,21 +51,35 @@ class SeDASAPI:
         try:
             self._token = json.load(urlopen(req))['token']
             self.headers['Authorization'] = f"Token {self._token}"
+            self._token_time = time.time()
         except HTTPError as e:
-            print(e)
-            print(e.read().decode())
+            # Note: This doesn't use the default error handling because that will try and log in again if you have
+            # provided invalid login details and would get stuck in an infinite loop
+            _logger.error(e)
+            _logger.error(e.read().decode())
             raise e
 
-    def search(self, _wkt, _start_date, _end_date, _sensor='All', **_filters):
+    def search(
+            self,
+            _wkt: str,
+            _start_date: str,
+            _end_date: str,
+            _sensor: str = 'All',
+            _retry: bool = True,
+            **_filters
+    ) -> dict:
         """
         Search the SeDAS system for products with the given parameters.
         :param _wkt: wkt formatted aoi
         :param _start_date: start date of search in ISO8601 format
         :param _end_date: end date of search in ISO8601 format
         :param _sensor: the type of data to search for.  Accepts All, SAR or Optical.  Defaults to All
+        :param _retry: should the request be retried if it fails.
         :param _filters: filter search on
         :return: list of search results
         """
+        self.login()
+
         query = {
             'sensorFilters': {"type": _sensor},
             'filters': _filters,
@@ -66,17 +87,16 @@ class SeDASAPI:
             'start': _start_date,
             'stop': _end_date
         }
-    
+
         req = Request(self.search_url, json.dumps(query).encode(), headers=self.headers)
         try:
             resp = urlopen(req)
             return json.load(resp)
         except HTTPError as e:
-            print(e)
-            print(e.read().decode())
-            raise e
+            if self._error_handling(e) and _retry:
+                return self.search(_wkt, _start_date, _end_date, _sensor, _retry=False, **_filters)
 
-    def search_sar(self, _wkt, _start_date, _end_date, **_filters):
+    def search_sar(self, _wkt: str, _start_date: str, _end_date: str, **_filters: dict) -> dict:
         """
         Search the SeDAS system for SAR products only with the given parameters
         :param _wkt: wkt formatted aoi
@@ -87,7 +107,7 @@ class SeDASAPI:
         """
         return self.search(_wkt, _start_date, _end_date, 'SAR', **_filters)
 
-    def search_optical(self, _wkt, _start_date, _end_date, **_filters):
+    def search_optical(self, _wkt: str, _start_date: str, _end_date: str, **_filters: dict) -> dict:
         """
         Search the SeDAS system for Optical products only with the given parameters
         :param _wkt: wkt formatted aoi
@@ -98,67 +118,79 @@ class SeDASAPI:
         """
         return self.search(_wkt, _start_date, _end_date, 'Optical', **_filters)
 
-    def search_product(self, _product_id):
+    def search_product(self, _product_id: str, _retry=True) -> dict:
         """
         Search for information about a known product id.
         :param _product_id: product id to look for
+        :param _retry: Should the request be retried on error.
         :return: search result dictionary
         """
+        self.login()
         url = f"{self.search_url}/products?ids={_product_id}"
         req = Request(url, headers=self.headers)
         try:
             with urlopen(req) as resp:
                 return json.load(resp)
         except HTTPError as e:
-            print(e)
-            print(e.read().decode())
-            raise e
+            if self._error_handling(e) and _retry:
+                return self.search_product(_product_id, _retry=False)
 
-    def download(self, _product, _output_path):
+    def download(self, _product, _output_path: str, _retry=True) -> None:
         """
-        download a product from sedas
+        Download a product from sedas
         :param _product: product dictionary from a search
         :param _output_path: where to put the output file
+        :param _retry: Should the request be retried on error.
         :return: None
         """
-    
+        with self.download_request(_product, _retry) as resp:
+            with open(_output_path, "+wb") as f:
+                shutil.copyfileobj(resp, f)
+
+    def download_request(self, _product: dict, _retry=True):
+        """
+        Download a product from sedas, returning the request object.
+        Use this over the download function when you don't want the data to touch disk before you do something with it.
+        :param _product: product dictionary from a search
+        :param _retry: Should the request be retried on error.
+        :return: response object that can be read to download the file.
+        """
+        self.login()
         url = _product['downloadUrl']
         if not url:
             raise AttributeError("no download url defined for product")
         req = Request(url, headers=self.headers)
         try:
-            with urlopen(req) as resp:
-                # TODO: consider a version of this function that returns the resp object so it doesn't have to touch the
-                #  disk in cases where that is better
-                with open(_output_path, "+wb") as f:
-                    shutil.copyfileobj(resp, f)
+            return urlopen(req)
         except HTTPError as e:
-            print(e)
-            print(e.read().decode())
-            raise e
+            if self._error_handling(e) and _retry:
+                return self.download_request(_product, _retry=False)
 
-    def request(self, _product):
+    def request(self, _product, _retry=True) -> str:
         """
         Request a file from the SeDAS long term archive
         :param _product: product to request from the search
+        :param _retry: Should the request be retried on error.
         :return: Request ID
         """
+        self.login()
         url = f"{self.base_url}/request/{_product['supplierId']}"
         req = Request(url, headers=self.headers, method="POST")
         try:
             resp = urlopen(req)
             return json.load(resp)['requestId']
         except HTTPError as e:
-            print(e)
-            print(e.read().decode())
-            raise e
+            if self._error_handling(e) and _retry:
+                return self.request(_product, _retry=False)
 
-    def is_request_ready(self, _request_id):
+    def is_request_ready(self, _request_id: str, _retry=True):
         """
         checks on the status of a request. If it is complete it will return the download url
         :param _request_id: request id to check on.
+        :param _retry: Should the request be retried on error.
         :return: download url if the request is complete, None otherwise
         """
+        self.login()
         url = f"{self.base_url}/request?ids={_request_id}"
         req = Request(url, headers=self.headers)
         try:
@@ -167,40 +199,21 @@ class SeDASAPI:
                 return decoded[0]['downloadUrl']
             return None
         except HTTPError as e:
-            print(e)
-            print(e.read().decode())
-            raise e
+            if self._error_handling(e) and _retry:
+                return self.is_request_ready(_request_id, _retry=False)
 
+    def _error_handling(self, error: HTTPError) -> bool:
+        """
+        Handle errors. If this is a recoverable error by trying again this will return true.
+        :param error: the HTTPError
+        :return: true if this error can be recovered by logging in again.
+        """
+        # if we have an authentication error try and login again and then try again.
+        if error.code == 403 or error.code == 401:
+            self._token = None
+            self.login()
+            return True
+        _logger.error(error)
+        _logger.error(error.read().decode())
+        raise error
 
-if __name__ == '__main__':
-    wkt = "POLYGON ((-78.0294047453918 7.54828534191209," \
-          "-75.5410318208992 4.9335544228762," \
-          "-73.5283895711597 6.84893487157956," \
-          "-76.0167624956523 9.46366579061545," \
-          "-78.0294047453918 7.54828534191209))"
-    startDate = "2017-04-30T00:00:00Z"
-    endDate = "2017-05-12T23:59:59Z"
-
-    output_path = "/tmp/"
-
-    _username = input("Please enter your username:")
-    __password = getpass("Please enter your password:")
-
-    sedas = SeDASAPI(_username, __password)
-
-    print("search by aoi and sensor type...")
-    result = sedas.search(wkt, startDate, endDate, "SAR")
-    print(json.dumps(result, sort_keys=True, indent=4, separators=(',', ': ')))
-
-    print("single product query...")
-    singleProduct = sedas.search_product("S1B_IW_GRDH_1SDV_20190528T105030_20190528T105055_016443_01EF3E_5E4F")
-    print(json.dumps(singleProduct, sort_keys=True, indent=4, separators=(',', ': ')))
-
-    print("Downloading results of aoi search...")
-
-    downloader = SeDASBulkDownload(sedas, output_path, parallel=3, verbose=True)
-    downloader.add(result['products'])
-    while not downloader.is_done():
-        time.sleep(5)
-    downloader.shutdown()
-    print("Download complete!")
